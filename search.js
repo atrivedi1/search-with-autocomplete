8a106235f4d634abcc7efa07c423e631b3070405
Akash Trivedi <akash@citizencode.io>
2017-01-14 23:17:53 -0800

    Implemented caching system

--- search.js
+++ search.js
@@ -1,9 +1,33 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     //assumptions
     var apiUrl = "https://api.viki.io/v4/search.json?c=b&per_page=5&with_people=true&with_paywall=1&app=100266a&t=1440586215";
     var appId = "100266a";
     var resultsCap = "5";
     var withPeople = true;
+
+    //cache
+    var cache = {};
+
+    //current matching results
+    var currentMatchingResults = [];
+
+    //helper functions
+    var findMatchingResults = function(searchTerm) {
+        console.log("trying to find matching results")
+        //clear results
+        currentMatchingResults = [];
+
+        //find matching results
+        for(var title in cache) {
+            var titleContainsCharacters = title.indexOf(searchTerm) > -1 ? true : false;
+     
+            if(titleContainsCharacters) {
+                var matchingImageInfo = {}
+                matchingImageInfo[title] = cache[title];
+                currentMatchingResults.push(matchingImageInfo)
+            }
+        }
+    };
 
     var generateSearchParams = function(searchTerm){
         var params = {
@@ -17,20 +41,38 @@ $( document ).ready(function() {
         return params;
     }
 
-    var makeGetRequest = function(searchParams) {
+    var getDataFromApi = function(searchParams) {
+        var searchTerm = searchParams.c; 
+
         $.get(apiUrl, function(data) {
-            console.log(data);
-            alert( "search was performed" );
+            console.log("search performed successfully")
+            
+            data.forEach(function(imageData){
+                var imageTitle = imageData.tt.trim().toLowerCase();
+                var imageUrl = imageData.i;
+                cache[imageTitle] = imageUrl;
+            });
+
+            findMatchingResults(searchTerm);
         });
     };
     
-    var searchApi = function(searchTerm) {
-        var searchParams = generateSearchParams(searchTerm);
-        makeGetRequest(searchParams);
+    var searchTitlesAndDisplayResults = function(searchTerm) {
+        //if cache is empty then get data from API;
+        if(Object.keys(cache).length === 0) {
+            var searchParams = generateSearchParams(searchTerm);
+            getDataFromApi(searchParams);
+        }
+        //once cache is created, find matching titles
+        else { findMatchingResults(searchTerm) }
+
+        //display results
+        
     };
 
-    $( "#search-input" ).keyup(function(e) {
-        var currentValueInSearchField = e.target.value;
-        return searchApi(currentValueInSearchField)
+    //event listeners/handlers
+    $("#search-input").keyup(function(e) {
+        var currentValueInSearchField = e.target.value.toLowerCase();
+        return searchTitlesAndDisplayResults(currentValueInSearchField)
     });
 });
