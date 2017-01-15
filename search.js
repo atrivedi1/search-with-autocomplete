42ba25202c2cc8a09128b28eb8b51725f5a9434b
Akash Trivedi <akash@citizencode.io>
2017-01-14 20:38:05 -0800

    Attached event listener to search input field

--- search.js
+++ search.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+$( document ).ready(function() {
+
+    var printValueInSearchField = function(text) {
+        console.log(text);
+    }
+
+    $( "#search-input" ).keyup(function(e) {
+        var currentValueInSearchField = e.target.value;
+        return printValueInSearchField(currentValueInSearchField)
+    });
+});
